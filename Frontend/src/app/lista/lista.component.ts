d81b9d77b66982c41db62c268f0607fd7b6cc758
domingospaes <domingospaes@hotmail.com>
2018-11-27 19:55:14 -0200

    ok

--- Frontend/src/app/lista/lista.component.ts
+++ Frontend/src/app/lista/lista.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ListaItem } from './listaitem';
 import { Bookdescription } from '../Services/bookdescription';
 import { BookdescriptionService } from '../Services/bookdescription.service';
+import { getRandomString } from 'selenium-webdriver/safari';
 
 @Component({
   selector: 'app-lista',
@@ -13,6 +14,7 @@ export class ListaComponent {
   public a: Bookdescription;
   itensBase: Bookdescription[];
   itens = new Array<ListaItem>();
+  itensTelaInicial = new Array<ListaItem>();
 
   constructor(private servBookDesk: BookdescriptionService) {
   }
@@ -23,8 +25,27 @@ export class ListaComponent {
       for (let entry of itens) {
         this.itens.push(new ListaItem(entry.ISBN, entry.title, [], entry.description, entry.price, entry.publisher, entry.pubdate, entry.edition, entry.pages));
       }
+      this.listaAleatoria();
     });
 
+  }
+
+  listaAleatoria() {
+    let aleatorio1 = Math.floor(Math.random() * this.itens.length);
+    this.itensTelaInicial.push(this.itens[aleatorio1]);
+    this.itens.splice(aleatorio1, 1);
+
+    let aleatorio2 = Math.floor(Math.random() * this.itens.length);
+    this.itensTelaInicial.push(this.itens[aleatorio2]);
+    this.itens.splice(aleatorio2, 1);
+
+    let aleatorio3 = Math.floor(Math.random() * this.itens.length);
+    this.itensTelaInicial.push(this.itens[aleatorio3]);
+    this.itens.splice(aleatorio3, 1);
+
+    this.itens.push(this.itensTelaInicial[0]);
+    this.itens.push(this.itensTelaInicial[1]);
+    this.itens.push(this.itensTelaInicial[2]);
   }
 
   descricaoResumida(descricao: String): String {
